9dab10415d44576411947f8dfd36327c209421c5
Mitch Steffens <mdsteffens1@gmail.com>
2018-10-10 16:53:31 -0400

    A03 Completed
    Finished Assignment 3 - LERP

--- A03 - LERP/AppClass.cpp
+++ A03 - LERP/AppClass.cpp
@@ -1,8 +1,9 @@
 #include "AppClass.h"
+
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Mitch Steffens - mds4685@rit.edu";
 	
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -38,26 +39,24 @@ void Application::InitVariables(void)
 		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
 		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
 
-		//??? static or not ???
-		static std::vector<vector3> stopList;
-
+		std::vector<vector3> stopList;
 		float angle = 2.0f * PI / i;  //determine angle between each point
-		std::cout << angle * 180/PI << std::endl;
-		for (uint p = 0; p < i; p++) {
+		for (uint p = 0; p <= i; p++) {
 			float currentAngle = angle * p;
-			vector3 currentPoint = vector3(cosf(currentAngle) * fSize, 0, sinf(currentAngle) * fSize);
-			stopList.push_back(currentPoint);
+			vector3 currentPoint = vector3(cosf(currentAngle) * fSize, sinf(currentAngle) * fSize, 0); //determine position of the point in world space
+			stopList.push_back(currentPoint); //add the points of this torus to the current vector
 		}
-
-		std::cout << "List: " << i << std::endl;
-		for (uint c = 0; c < stopList.size(); c++) {
-			std::cout << "x:" << stopList[c].x << " y:" <<stopList[c].y << " z:" <<stopList[c].z << std::endl;
-		}
+		stopLists.push_back(stopList); //add the vector for this torus to the vector of vectors
 
 		fSize += 0.5f; //increment the size for the next orbit
 		uColor -= static_cast<uint>(decrements); //decrease the wavelength
 	}
 
+
+	pointCounter = new int[m_uOrbits];
+	for (uint i = 0; i < m_uOrbits; i++) {
+		pointCounter[i] = 0;
+	}
 }
 void Application::Update(void)
 {
@@ -70,6 +69,7 @@ void Application::Update(void)
 	//Is the first person camera active?
 	CameraRotation();
 }
+
 void Application::Display(void)
 {
 	// Clear the screen
@@ -81,15 +81,42 @@ void Application::Display(void)
 	/*
 		The following offset will orient the orbits as in the demo, start without it to make your life easier.
 	*/
-	//m4Offset = glm::rotate(IDENTITY_M4, 1.5708f, AXIS_Z);
+	m4Offset = glm::rotate(IDENTITY_M4, 1.5708f, AXIS_Z);
 
 	// draw a shapes
+	static float fTimer = 0;
+	static uint uClock = m_pSystem->GenClock();
+	fTimer += m_pSystem->GetDeltaTime(uClock);
+	float fTotalAnimationTime = 3.0f;
+	float fPercentage;
+
 	for (uint i = 0; i < m_uOrbits; ++i)
 	{
 		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 1.5708f, AXIS_X));
 
 		//calculate the current position
 		vector3 v3CurrentPos = ZERO_V3;
+
+		fPercentage = fTimer - (fTotalAnimationTime * pointCounter[i]) / fTotalAnimationTime;
+
+		// increment the counter for this circle
+		if (fPercentage >= 1.0f) {
+			pointCounter[i]++;
+		}
+
+		std::vector<vector3> currentList = stopLists[i];
+		int currentCount = pointCounter[i];
+
+		//if (pointCounter[i] < currentList.size() - 1) {
+		//	v3CurrentPos = glm::lerp(currentList[currentCount], currentList[currentCount + 1], fPercentage);
+		//}
+		//else {
+		//	v3CurrentPos = glm::lerp(currentList[currentCount % (i+3)], currentList[currentCount % (i + 3) + 1], fPercentage)/*currentList[currentList.size() - 1]*/;
+		//}
+
+		v3CurrentPos = glm::lerp(currentList[currentCount % (i + 3)], currentList[currentCount % (i + 3) + 1], fPercentage)/*currentList[currentList.size() - 1]*/;
+
+
 		matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
 
 		//draw spheres
@@ -110,6 +137,7 @@ void Application::Display(void)
 }
 void Application::Release(void)
 {
+	delete[] pointCounter;
 	//release GUI
 	ShutdownGUI();
 }
--- A03 - LERP/AppClass.h
+++ A03 - LERP/AppClass.h
@@ -18,6 +18,8 @@ Date: 2017/05
 
 class Application
 {
+	std::vector<std::vector<vector3>> stopLists;
+	int* pointCounter;
 	uint m_uOrbits = 0; //number of shapes starting at 3 and increasing in sides
 	std::vector<uint> m_shapeList; //shape index for circles
 	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
